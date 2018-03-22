ee6b37577ca61eedcbd9dc41ce7ee0ff5c654a31
gsum <gsum@192.168.33.1>
2018-03-22 16:17:27 -0400

    Set console cursor position & color

---
+++ README.md
@@ -0,0 +1,7 @@
+
+All examples are personal use and references only, you take risk of using them on your own.
+
+### Node(v8.9.4) Example
+* [1](/node/1) - Set console cursor position & color
+
+Edit ReadMe: https://dillinger.io/
---
+++ node/1/stock.js
@@ -0,0 +1,31 @@
+// Import socket.io with a connection to a channel (i.e. tops)
+const socket = require('socket.io-client')('https://ws-api.iextrading.com/1.0/tops')
+require ('colors')
+const _ = require('lodash');
+var readline = require('readline');
+
+function stale() {
+  readline.cursorTo(process.stdout, 40);
+  process.stdout.write("++++".bold.red)
+}
+// Listen to the channel's messages
+socket.on('message', message => {
+  message = JSON.parse(message)
+  readline.clearLine(process.stdout, 0);  // clear current text
+  readline.cursorTo(process.stdout, 0);  // move cursor to the beginning of line
+  process.stdout.write(message.symbol+':bidPrice:'+ message.bidPrice+',askPrice'+message.askPrice);
+  readline.cursorTo(process.stdout, 40);
+  process.stdout.write("----".yellow);
+  _.debounce(stale, 1000)();
+})
+
+// Connect to the channel
+socket.on('connect', () => {
+
+  // Subscribe to topics (i.e. appl,fb,aig+)
+  socket.emit('subscribe', 'uco')
+
+})
+
+// Disconnect from the channel
+socket.on('disconnect', () => console.log('Disconnected.'))
